c7f645b142a33de3d9de471d7a500ab4586720e5
shapkina-l <shapkina199@gmail.com>
2025-08-03 22:32:39 +0200

    sandbox

---
+++ level1/sandbox/sandbox.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+static volatile sig_atomic_t timeout_flag = 0;
+
+void handle_alarm(int sig)
+{
+	(void)sig;
+	timeout_flag = 1;
+}
+
+int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+{
+	pid_t	cpid;
+	pid_t	ret;
+	int		status;
+	int		exit_code;
+	struct sigaction sa;
+
+	sa.sa_handler = handle_alarm;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGALRM, &sa, NULL) == -1)
+		return (-1);
+
+	cpid = fork();
+	if (cpid == -1)
+	{
+		return (-1);
+	}
+	if (cpid == 0)
+	{
+		f();
+		exit(0);
+	}
+	alarm(timeout);
+	while (1)
+	{
+		ret = waitpid(cpid, &status, 0);
+		if (ret == cpid)
+		{
+			if (WIFEXITED(status))
+			{
+				exit_code = WEXITSTATUS(status);
+				if (exit_code == 0)
+				{
+					if (verbose)
+						printf("Nice function\n");
+					return (0);
+				}
+				else 
+				{
+					if (verbose)
+						printf("Bad function: exited with code %d\n", exit_code);
+					return (1);
+				}
+			}
+			else if (WIFSIGNALED(status))
+			{
+				if (verbose)
+					printf("Bad function: %s\n", strsignal(WTERMSIG(status)));
+				return (0);
+			}
+		}
+		else if (timeout_flag)
+		{
+			kill(cpid, SIGKILL);
+			waitpid(cpid, &status, 0);
+			if (verbose)
+				printf("Bad funciton: timed out after %d seconds\n", timeout);
+			return (0);
+		}
+		usleep(1000);
+	}
+	return (-1);
+}
